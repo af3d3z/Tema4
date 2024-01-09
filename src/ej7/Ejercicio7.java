b7ef5a107c7f4c1c16d34d41d389659cd2609f21
Alonso Fernández <alonso.fernandez@iesnervion.es>
2024-01-09 14:39:41 +0100

    Initial commit

--- src/ej7/Ejercicio7.java
+++ src/ej7/Ejercicio7.java
@@ -1,4 +1,19 @@
 package ej7;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        // guardará la temperatura media de cada mes del año
+        int[] tempMedia = new int[12];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < tempMedia.length; i++){
+            System.out.print("Ingresa la temperatura media del mes " + i + ":");
+            tempMedia[i] = sc.nextInt();
+        }
+
+        System.out.println("TEMPERATURA MEDIA DEL MES:");
+        for(int i = 0; i)
+    }
 }
